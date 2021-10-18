e7dbf4b3e78f568ae711eb260260fa6dc78d09a6
Roseoketch <oketchrose@gmail.com>
2021-10-18 16:49:34 +0300

    Adding the the message according to the expectation

--- days_calc.py
+++ days_calc.py
@@ -17,11 +17,11 @@ def get_valid_date (prompt):
         break
     return value
 
-
+ 
 def days_difference_calculator ():
-    start_date = get_valid_date('Type the start date in ‘YYYY-MM-DD’ format: ')
-    end_date = get_valid_date('Type the end date in ‘YYYY-MM-DD’ format: ')
+    start_date = get_valid_date('Enter the start date in the ‘YYYY-MM-DD’ format: ')
+    end_date = get_valid_date('Enter the end date in ‘YYYY-MM-DD’ format: ')
     days_difference = abs((end_date - start_date).days)
-    print ('The difference in days between the two dates is ' + str(days_difference) + ' day(s).')
+    print ('The days difference is ' + str(days_difference) + ' day(s).')
     
 days_difference_calculator()
--- even_no.py
+++ even_no.py
@@ -9,25 +9,25 @@ import ast
 def even_numbers_in_list():
     even_numbers_list = []
     try:
-        list_input = ast.literal_eval(input('Paste your list here: '))
+        list_input = ast.literal_eval(input('Add your list here: '))
         while len (list_input) == 0:
-            print ('The list is empty, paste a list of integers.')
+            print ('The list is empty, kindly input numbers.')
             list_input = ast.literal_eval(input('Paste your list here: '))
             
         if all(isinstance(item, int) for item in list_input):
-            print ('List of even numbers in the list:')
+            print ('From the list give, the following re the even numbers:')
             for item in list_input:
                 if item % 2 == 0:
                     even_numbers_list.append(item)
             if len (even_numbers_list) > 0:
                 print (even_numbers_list)
             else:
-                print ('There is no even number in the inputted list.')
+                print ('Sorry, from the list, there is no even number.')
         else:
             print ('There are non-integer value(s) in the list, ensure all values are integers.')
             even_numbers_in_list()
     except (ValueError, SyntaxError):
-        print ('What you inputted is not a list of integers. An example of list of integers is [1,2,3,4,5,6]')
+        print ('Ensure you add correct list of integers. i.e [1,2,3,4,5,6]')
         even_numbers_in_list()
         
 even_numbers_in_list()
